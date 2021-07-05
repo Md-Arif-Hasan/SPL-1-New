a4b049b0faa4815688e9e54c021ef42c4539e972
Md Arif Hasan <48629132+Md-Arif-Hasan@users.noreply.github.com>
2021-07-05 21:47:32 +0600

    Testing input file

---
+++ raw.cpp
@@ -0,0 +1,15 @@
+      int i=0;
+      int j =10;
+      if(i>2)
+      {
+            cout<<"a";
+      }
+      else
+      {
+            k = j;
+      }
+      i =2;
+      cout <<i<<endl;
+
+
+
