8f470bedaded1898113afc48db1d26c9575715a8
Md Arif Hasan <48629132+Md-Arif-Hasan@users.noreply.github.com>
2021-08-26 01:14:15 +0600

    Source code - 9 ( for within while )

---
+++ raw9.cpp
@@ -0,0 +1,13 @@
+{
+    int i =0;
+
+    while ( true )
+    {
+        for(  i  = 1 ;  i  <= 10;  i++)
+        {
+                cout<<" FOR LOOP"<<endl;
+        }
+    }
+    cout<< i << endl;
+}
+
