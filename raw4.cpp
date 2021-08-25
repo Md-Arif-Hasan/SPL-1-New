9a3ef0affef9a6f5d3a815d76bd793f11f565009
Md Arif Hasan <48629132+Md-Arif-Hasan@users.noreply.github.com>
2021-08-26 00:02:54 +0600

    Source Code-2 (if..else if)

---
+++ raw4.cpp
@@ -0,0 +1,16 @@
+{
+        int start = 20, ends =10;
+        if (start > ends)
+        {
+                cout <<" SUCCESSFULL" <<endl;
+        }
+        else if
+        {
+                cout <<"Unsuccessful" <<endl;
+        }
+        else
+        {
+                cout <<" Nothing "<<endl;
+        }
+        cout << start <<endl;
+}
