7a78432bb66a2de2312a6b36b20c8634edcf61f0
Md Arif Hasan <48629132+Md-Arif-Hasan@users.noreply.github.com>
2021-08-26 01:09:00 +0600

    source code - 4 ( nested while loop )

---
+++ raw8.cpp
@@ -0,0 +1,11 @@
+{
+        int i =0;
+        while ( true )
+        {
+                while ( i >= 0)
+                {
+                         i = i - 1;
+                }
+        }
+        cout << i << endl;
+}
